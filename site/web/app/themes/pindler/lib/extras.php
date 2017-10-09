2e356d4ff1297b50bc0802f6f70282d1a06fba05
Mike <m.walcott33@gmail.com>
2017-10-09 16:22:24 -0600

    made li style itralic

--- site/web/app/themes/pindler/lib/extras.php
+++ site/web/app/themes/pindler/lib/extras.php
@@ -52,7 +52,7 @@ if( function_exists('acf_add_options_page') ) {
 		'menu_title' 	=> 'Theme Options',
 		'menu_slug' 	=> 'theme-options',
 		'capability' 	=> 'edit_posts',
-		'position' => 25,
+		'position' => 26,
 		'icon_url' => 'dashicons-schedule',
 		'redirect' 	=> false
 	));
---
+++ site/web/app/themes/pindler/template-resources.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Template Name: Showrooms
+ */
+?>
+
+<?php get_template_part('templates/content', 'showrooms'); ?>
